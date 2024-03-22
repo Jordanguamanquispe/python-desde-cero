62c1a944a1aac809686692dabd2cc35db7b6f920
jeff_853 <144867971+Jordanguamanquispe@users.noreply.github.com>
2024-03-22 15:20:36 -0500

    Update practica.py

--- jeff.0/python/python desde cero/practica.py
+++ jeff.0/python/python desde cero/practica.py
@@ -2,23 +2,23 @@
 #imprima un contador regresivo desde ese número hasta 1.
 #ejercicio 1 
 
-#print("contador")
-#contador = int(input("escriba un numero: "))
-#while contador >= 1 :
-    #print(contador)
-    #contador -= 1 
-#print("fin")
+print("contador")
+contador = int(input("escriba un numero: "))
+while contador >= 1 :
+    print(contador)
+    contador -= 1 
+print("fin")
 
 # haga un programa que el usuario ingrese la cantidad de productos y sume los precios 
-#print("Tienda Tuti")
-#num_productos = int(input("Escriba la cantidad de productos: "))
-#contador = 0
-#print("Escriba los precios de cada producto:")
-#while num_productos > 0:
-    #precio = float(input("Precio del producto: "))
-    #contador += precio
-   # num_productos -= 1
-#print("El precio total es:", contador)
+print("Tienda Tuti")
+num_productos = int(input("Escriba la cantidad de productos: "))
+contador = 0
+print("Escriba los precios de cada producto:")
+while num_productos > 0:
+    precio = float(input("Precio del producto: "))
+    contador += precio
+    num_productos -= 1
+print("El precio total es:", contador)
 
 #JORDAN SI VUELVES A LEER ESTO RECUERDA QUE SI VAS HACER TIPO CONTADOR RECUERDA PONER CONTADOR 
 #PRIMERO Y DESPUES ESCRIBE QUE SEA MAYOR A CERO ESTO TE SERVIRA PARA 
@@ -26,16 +26,16 @@
 #PONGAS CONTADOR -= 1 
 
 #tabla de multiplicar 
-#print("tabla de multiplicar")
-#num1 = int(input("escriba la tabla que desea imprimir en pantalla: "))
-#long = int(input("escriba hasta donde quiere que se multiplique la tabla: "))
-#x = 0
-#resultado = 0 
-#while long >= x :
-    #print( "{} x {} = {}".format(num1 , x , resultado)  )
-   # x += 1
-    #resultado = num1 * x 
-#print("fin")
+print("tabla de multiplicar")
+num1 = int(input("escriba la tabla que desea imprimir en pantalla: "))
+long = int(input("escriba hasta donde quiere que se multiplique la tabla: "))
+x = 0
+resultado = 0 
+while long >= x :
+    print( "{} x {} = {}".format(num1 , x , resultado)  )
+    x += 1
+    resultado = num1 * x 
+print("fin")
 
 
   
